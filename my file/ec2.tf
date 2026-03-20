943195eef84380e747990732c5970fc088155e0d
Abhishek Das <abhishekred60@gmail.com>
2026-03-20 17:21:27 +0530

    ss

--- my file/ec2.tf
+++ my file/ec2.tf
@@ -5,8 +5,6 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-076742b894530ab1f"  
   instance_type = "t3.small"
-  key_name = "pair-key"   # 
-  vpc_security_group_ids = ["sg-03bd0f9b90c633808"]
   tags = {
     Name = "MyTerraformEC2"
   }
